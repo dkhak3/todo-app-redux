3cd01711881d6c29a1f8c86b0bb7094f494a50e1
Nguyễn Hữu Duy Kha <94631848+dkhak3@users.noreply.github.com>
2023-12-01 20:07:16 +0700

    REDUX TOOLKIT cấu hình store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,26 @@
-import { createStore } from "redux";
-import rootReducer from "./reducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// ========= REDUX CORE =========
 
-const composedEnhancers = composeWithDevTools();
+// import { createStore } from "redux";
+// import rootReducer from "./reducer";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, composedEnhancers);
+// const composedEnhancers = composeWithDevTools();
+
+// const store = createStore(rootReducer, composedEnhancers);
+
+// export default store;
+
+// ========= REDUX TOOLKIT =========
+
+import { configureStore } from "@reduxjs/toolkit";
+import filtersReducer from "../components/Filters/FiltersSlice";
+import todoListReducer from "../components/TodoList/TodosSlice";
+
+const store = configureStore({
+  reducer: {
+    filters: filtersReducer,
+    todoList: todoListReducer,
+  },
+});
 
 export default store;
